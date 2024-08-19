0b522521232266e046a6bc0add5ca5eea02231cb
Alec Merdler <alecmerdler@gmail.com>
2024-08-19 11:46:36 -0400

    Update Dedicated metrics example
    Use an environment variable for the target endpoint so that
    customers can easily override it for their own setup.

--- authzed-dedicated-metrics/otel-collector/env-vars.sh
+++ authzed-dedicated-metrics/otel-collector/env-vars.sh
@@ -1,3 +1,4 @@
+export AUTHZED_DEDICATED_HOST='<your-authzed-dashboard-url>'
 export PROMETHEUS_USERNAME='<permission-system-name>'
 export PROMETHEUS_PASSWORD='<permission-system-token>'
 export OTEL_EXPORTER_OTLP_ENDPOINT="https://<metrics-service-url>:443"
